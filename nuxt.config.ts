f166b198e95e9b9a428d59b3f4f0ef1bc6564d71
SONE-Mei <185347250+sone-meidai@users.noreply.github.com>
2025-06-27 13:18:03 +0700

    init

---
+++ nuxt.config.ts
@@ -0,0 +1,18 @@
+import tailwindcss from "@tailwindcss/vite";
+
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: "2025-05-15",
+  devtools: { enabled: true },
+  css: ["~/assets/css/tailwind.css"],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
+
+  modules: ["shadcn-nuxt"],
+  shadcn: {
+    prefix: '',
+    componentDir: './components/ui'
+  }
+});
